f07a6a716e8c7c01fceb1145561ab06983d552cd
lvzhuo <lvzhuo@yfcloud.com>
2018-09-11 17:55:08 +0800

    modify random

--- random/rand.go
+++ random/rand.go
@@ -1,7 +1,6 @@
 package random
 
 import (
-	"math"
 	"math/rand"
 	"time"
 )
@@ -10,144 +9,80 @@ var (
 	defaultRand = rand.New(rand.NewSource(time.Now().UnixNano()))
 )
 
-// Creates a random string based on a variety of options, using
-// supplied source of randomness.
-//
-// If start and end are both 0, start and end are set
-// to ' ' and 'z', the ASCII printable
-// characters, will be used, unless letters and numbers are both
-// false, in which case, start and end are set to 0 and math.MaxInt32.
-//
-// If set is not nil, characters between start and end are chosen.
-//
-// This method accepts a user-supplied rand.Rand
-// instance to use as a source of randomness. By seeding a single
-// rand.Rand instance with a fixed seed and using it for each call,
-// the same random sequence of strings can be generated repeatedly
-// and predictably.
-func Spec0(count uint, start, end int, letters, numbers bool,
-	chars []rune, rand *rand.Rand) string {
-	if count == 0 {
-		return ""
+const (
+	Digits       = "0123456789"
+	LowerLetters = "abcdefghijklmnopqrstuvwxyz"
+	UpperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+	SpecialChars = "!@#$%&*+-=?"
+
+	Space = " "
+)
+
+func Chars(length int) string {
+	s := Digits + LowerLetters + UpperLetters
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = s[rand.Int63()%int64(len(s))]
 	}
-	if start == 0 && end == 0 {
-		end = 'z' + 1
-		start = ' '
-		if !letters && !numbers {
-			start = 0
-			end = math.MaxInt32
-		}
+	return string(b)
+}
+
+// Password will generate a random password
+// Minimum number length of 5 if less than
+func CustomChars(lower bool, upper bool, numeric bool, special bool, space bool, num int) string {
+	// Make sure the num minimun is at least 5
+	if num < 5 {
+		num = 5
 	}
-	buffer := make([]rune, count)
-	gap := end - start
-	for count != 0 {
-		count--
-		var ch rune
-		if len(chars) == 0 {
-			ch = rune(rand.Intn(gap) + start)
-		} else {
-			ch = chars[rand.Intn(gap)+start]
-		}
-		if letters && ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) ||
-			numbers && (ch >= '0' && ch <= '9') ||
-			(!letters && !numbers) {
-			if ch >= rune(56320) && ch <= rune(57343) {
-				if count == 0 {
-					count++
-				} else {
-					buffer[count] = ch
-					count--
-					buffer[count] = rune(55296 + rand.Intn(128))
-				}
-			} else if ch >= rune(55296) && ch <= rune(56191) {
-				if count == 0 {
-					count++
-				} else {
-					// high surrogate, insert low surrogate before putting it in
-					buffer[count] = rune(56320 + rand.Intn(128))
-					count--
-					buffer[count] = ch
-				}
-			} else if ch >= rune(56192) && ch <= rune(56319) {
-				// private high surrogate, no effing clue, so skip it
-				count++
-			} else {
-				buffer[count] = ch
-			}
-		} else {
-			count++
-		}
+	i := 0
+	b := make([]byte, num)
+	var passString string
+
+	if lower {
+		passString += LowerLetters
+		b[i] = LowerLetters[rand.Int63()%int64(len(LowerLetters))]
+		i++
 	}
-	return string(buffer)
-}
+	if upper {
+		passString += UpperLetters
+		b[i] = UpperLetters[rand.Int63()%int64(len(UpperLetters))]
+		i++
+	}
+	if numeric {
+		passString += Digits
+		b[i] = Digits[rand.Int63()%int64(len(Digits))]
+		i++
+	}
+	if special {
+		passString += SpecialChars
+		b[i] = SpecialChars[rand.Int63()%int64(len(SpecialChars))]
+		i++
+	}
+	if space {
+		passString += Space
+		b[i] = Space[rand.Int63()%int64(len(Space))]
+		i++
+	}
 
-// Creates a random string whose length is the number of characters specified.
-//
-// Characters will be chosen from the set of alpha-numeric
-// characters as indicated by the arguments.
-//
-// Param count - the length of random string to create
-// Param start - the position in set of chars to start at
-// Param end   - the position in set of chars to end before
-// Param letters - if true, generated string will include
-//                 alphabetic characters
-// Param numbers - if true, generated string will include
-//                 numeric characters
-func Spec1(count uint, start, end int, letters, numbers bool) string {
-	return Spec0(count, start, end, letters, numbers, nil, defaultRand)
-}
+	// Set default if empty
+	if passString == "" {
+		passString = LowerLetters + Digits
+	}
 
-// Creates a random string whose length is the number of characters specified.
-//
-// Characters will be chosen from the set of alpha-numeric
-// characters as indicated by the arguments.
-//
-// Param count - the length of random string to create
-// Param letters - if true, generated string will include
-//                 alphabetic characters
-// Param numbers - if true, generated string will include
-//                 numeric characters
-func AlphaOrNumeric(count uint, letters, numbers bool) string {
-	return Spec1(count, 0, 0, letters, numbers)
-}
+	// Loop through and add it up
+	for i <= num-1 {
+		b[i] = passString[rand.Int63()%int64(len(passString))]
+		i++
+	}
 
-func String(count uint) string {
-	return AlphaOrNumeric(count, false, false)
-}
+	// Shuffle bytes
+	for i := range b {
+		j := defaultRand.Intn(i + 1)
+		b[i], b[j] = b[j], b[i]
+	}
 
-func StringSpec0(count uint, set []rune) string {
-	return Spec0(count, 0, len(set)-1, false, false, set, defaultRand)
-}
-
-func StringSpec1(count uint, set string) string {
-	return StringSpec0(count, []rune(set))
-}
-
-// Creates a random string whose length is the number of characters
-// specified.
-//
-// Characters will be chosen from the set of characters whose
-// ASCII value is between 32 and 126 (inclusive).
-func Ascii(count uint) string {
-	return Spec1(count, 32, 127, false, false)
-}
-
-// Creates a random string whose length is the number of characters specified.
-// Characters will be chosen from the set of alphabetic characters.
-func Alphabetic(count uint) string {
-	return AlphaOrNumeric(count, true, false)
-}
-
-// Creates a random string whose length is the number of characters specified.
-// Characters will be chosen from the set of alpha-numeric characters.
-func Alphanumeric(count uint) string {
-	return AlphaOrNumeric(count, true, true)
-}
-
-// Creates a random string whose length is the number of characters specified.
-// Characters will be chosen from the set of numeric characters.
-func Numeric(count uint) string {
-	return AlphaOrNumeric(count, false, true)
+	return string(b)
 }
 
 func Int(max int) int {
